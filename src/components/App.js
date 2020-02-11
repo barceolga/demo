ff625947b91fb6c3c9b0e5d060a593786bb4b744
Olga <olgafurmanowska@gmail.com>
2020-02-11 15:22:10 +0100

    Create components and basic App layout

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+
+const App = () => (
+  <div className="container-fluid">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/about" component={AboutPage}></Route>
+      <Route component={PageNotFound} />
+    </Switch>
+  </div>
+);
+
+export default App;
