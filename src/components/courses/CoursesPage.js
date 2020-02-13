49c62c450f4f4e625edc58df260e201cc32a5608
Olga <olgafurmanowska@gmail.com>
2020-02-13 10:53:30 +0100

    Convert CoursePage to be a container, add logic for showing created course

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
   state = {
@@ -9,13 +12,11 @@ class CoursesPage extends React.Component {
 
   handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
-    this.setState({
-      course
-    });
+    this.setState({ course });
   };
   handleSubmit = event => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   };
   render() {
     return (
@@ -28,9 +29,23 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
         <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={this.props.courses.indexOf(course)}>{course.title}</div>
+        ))}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
