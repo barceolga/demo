7d1a7c7f15dc97b24fef8abfaebcb34fb077db4f
Olga <olgafurmanowska@gmail.com>
2020-02-18 11:31:35 +0100

    Add Spinner for loading, pass onDeleteClick prop to child, add handleDeleteCourse,  deleteCourseAction

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,8 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 class CoursesPage extends React.Component {
   state = {
@@ -24,19 +26,40 @@ class CoursesPage extends React.Component {
       });
     }
   }
+  // Async await instead of simple promise.
+  handleDeleteCourse = async course => {
+    toast.success("Course deleted");
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
+      toast.error("Delete failed " + error.message, { autoClose: false });
+    }
+  };
+
   render() {
     return (
       <div>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.courses}></CourseList>
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            {" "}
+            {/*this is called fragment, and it's preferible to use this element instead of a div when wrapping up JSX syntax in single parent element*/}
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList
+              courses={this.props.courses}
+              onDeleteClick={this.handleDeleteCourse}
+            ></CourseList>
+          </>
+        )}
       </div>
     );
   }
@@ -45,7 +68,8 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
@@ -59,7 +83,8 @@ function mapStateToProps(state) {
               authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors
+    authors: state.authors,
+    loading: state.apiCallsInProgress > 0
   };
 }
 
@@ -67,7 +92,8 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
     }
   };
 }
