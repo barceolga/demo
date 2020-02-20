5b18ac8417908282dac3391d281ab2bbae9806e2
Olga <olgafurmanowska@gmail.com>
2020-02-20 07:51:04 +0100

    Add number of courses in Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { loadCourses } from "../../redux/actions/courseActions";
 
-const Header = () => {
+const Header = courses => {
   const activeStyle = { color: "#F15B2A" };
+  const coursesNumber = courses.courses.length;
 
   return (
     <nav>
@@ -17,8 +21,26 @@ const Header = () => {
       <NavLink to="/courses" activeStyle={activeStyle}>
         Courses
       </NavLink>
+      <div>
+        {coursesNumber === 0 ? null : (
+          <h3>Total courses number: {coursesNumber}</h3>
+        )}
+      </div>
     </nav>
   );
 };
+Header.propTypes = {
+  courses: PropTypes.array.isRequired
+};
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    courses: state.courses
+  };
+};
+
+const mapDisptachToProps = {
+  loadCourses
+};
+
+export default connect(mapStateToProps, mapDisptachToProps)(Header);
