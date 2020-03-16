0687e03a3731cf505fa149577403a3e4ed058c57
Olga Furmanowska <olgafurmanowska@gmail.com>
2020-03-16 08:20:11 +0100

    Add new action types, action creators and new cases to author reducer for adding and updating author

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -6,6 +6,14 @@ export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
 }
 
+export function createAuthorSuccess(author) {
+  return {type: types.CREATE_AUTHOR_SUCCESS, author}
+}
+
+export function updateAuthorSuccess(author) {
+  return {type: types.UPDATE_AUTHOR_SUCCESS, author}
+}
+
 export function loadAuthors() {
   return function(dispatch) {
     dispatch(beginApiCall());
@@ -13,6 +21,24 @@ export function loadAuthors() {
       .getAuthors()
       .then(authors => {
         dispatch(loadAuthorsSuccess(authors));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function saveAuthor(author) {
+  //eslint-disable-next-line no-unused-vars
+  return function(dispatch, getState) {
+    dispatch(beginApiCall());
+    return authorApi
+      .saveAuthor(author)
+      .then(savedAuthor => {
+        author.id
+          ? dispatch(updateAuthorSuccess(savedAuthor))
+          : dispatch(createAuthorSuccess(savedAuthor));
       })
       .catch(error => {
         dispatch(apiCallError(error));
--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -3,6 +3,12 @@ import initialState from "./initialState";
 
 export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
+    case types.CREATE_AUTHOR_SUCCESS:
+      return [...state, { ...action.author }];
+    case types.UPDATE_AUTHOR_SUCCESS:
+      return state.map(author =>
+        author.id === action.author.id ? action.author : author
+      );
     case types.LOAD_AUTHORS_SUCCESS:
       return action.authors;
     default:
